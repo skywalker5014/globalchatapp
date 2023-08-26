791dab81fae9baa6164b5fdf5471a8be9ced4e03
skywalker5014 <44sum77ukh@gmail.com>
2023-08-27 02:01:19 +0530

    added frontend design

--- client/src/Auth.jsx
+++ client/src/Auth.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const [user, setuser] = useState("");  //a state to capture the entered username
+  const [user, setuser] = useState(""); //a state to capture the entered username
   const [password, setpassword] = useState(""); //a state to capture the entered password
   const navigate = useNavigate(); //react router dom method used to redirect to specifed page
 
   let auth = { username: user, password: password }; //a js object to store the credentials entered
 
-  async function login() { //an asynchronous function to handle login
+  //an asynchronous function to handle login
+  async function login() {
     try {
       const response = await fetch("http://localhost:5050/login", {
         method: "POST",
@@ -17,9 +18,10 @@ function Home() {
         },
         body: JSON.stringify(auth),
       });
-      const result = await response.json(); //captured the response 
+      const result = await response.json(); //captured the response
 
-      if (result === "logged in") {  //if response is 'logged in' (for successful login)
+      if (result === "logged in") {
+        //if response is 'logged in' (for successful login)
         navigate("/chat", { replace: true }); //replace Home component with chat component
       } else {
         alert(result); //alert the response on failure of login
@@ -29,7 +31,8 @@ function Home() {
     }
   }
 
-  async function register() { //asynchronous function to handle registration
+  //asynchronous function to handle registration
+  async function register() {
     try {
       await fetch("http://localhost:5050/register", {
         method: "POST",
@@ -44,7 +47,7 @@ function Home() {
   }
 
   return (
-    <div>
+    <div id="authbackground">
       <div className="authcontainer">
         <input
           type="text"
@@ -60,10 +63,14 @@ function Home() {
           onChange={(e) => setpassword(e.target.value)}
           placeholder="password"
         />
-        <button className="btn" id="login" onClick={() => login()}>
+        <button className="btn auth-btn" id="login" onClick={() => login()}>
           login
         </button>
-        <button className="btn" id="register" onClick={() => register()}>
+        <button
+          className="btn auth-btn"
+          id="register"
+          onClick={() => register()}
+        >
           register
         </button>
       </div>
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,47 +1,79 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function Chat() {
   const [message, setmessage] = useState([]); //state to capture all the messages received
   const [datatosend, setdatatosend] = useState(""); //state to capture all the messages to be sent
   const [usrname, setusrname] = useState(""); //state tp capture the username after login
   const navigate = useNavigate(); //react router dom method used to redirect to specifed page
 
+  const websocket = new WebSocket("ws://localhost:5000"); //setup a new websocket connection
 
-  const websocket = new WebSocket("ws://localhost:5000"); //setup a new websocket connection  
-
-  useEffect(() => {//useeffect to synchronize data from the backend 
-    websocket.onmessage = (event) => { //listen to message event from the server and capture the message in the message event
-      if (event.data.startsWith("uname:")) { //extract the message event and (since first message is string username) use js string method startswith
+  //useeffect to synchronize data from the backend
+  useEffect(() => {
+    //listen to message event from the server and capture the message in the message event
+    websocket.onmessage = (event) => {
+      //extract the message event and (since first message is string username) use js string method "startswith"
+      if (event.data.startsWith("uname:")) {
         const username = event.data.slice(6);
         setusrname(username); //set username state
       } else {
         setmessage((prevMessages) => [...prevMessages, event.data]); //append new message the previous messages to the message state
+        console.log(message);
       }
     };
   }, []);
 
   function send() {
-    websocket.send(datatosend); //send data to the server
-    setdatatosend("")//reset the input field after sending the data
+    if (datatosend === "") {
+      return;
+    } else {
+      websocket.send(datatosend);
+      setdatatosend("");
+      document.getElementById("messagefield").value = "";
+    }
   }
 
   return (
     <>
-      <div className="messagecontainer">
-        {message.map((element, index) => (
-          <div className="messagebox" key={index}>{element}</div>
-        ))}
+      <nav>
+        <button
+          className="btn"
+          id="logout"
+          onClick={() => navigate("/", { replace: true })}
+        >
+          logout
+        </button>
+      </nav>
+      <div className="chatcontainer">
+        <div className="messagecontainer">
+          {message.map((element, index) => {
+            if (element !== "") {
+              return (
+                <div className="messagebox" key={index}>
+                  <i style={{ color: "white" }}>user@{element.split(":")[0]}</i>
+                  <br></br>
+                  {element.split(":")[1]}
+                </div>
+              );
+            } else {
+              return;
+            }
+          })}
+        </div>
+        <div className="useraccess">
+          <input
+            type="text"
+            id="messagefield"
+            onChange={(event) =>
+              setdatatosend(`${usrname}: ${event.target.value}`)
+            }
+          />
+          <button className="btn" id="msgsend" onClick={() => send()}>
+            <i className="material-icons">&#xe163;</i>
+          </button>
+        </div>
       </div>
-      <input
-        type="text" 
-        id="messagefield"
-        value={datatosend}
-        onChange={(event) => setdatatosend(`${usrname}: ${event.target.value}`)}
-      />
-      <button className="btn" id="msgsend" onClick={() => send()}>send</button>
-      <button className="btn" id="logout" onClick={() => navigate("/", { replace: true })}>logout</button>
     </>
   );
 }
